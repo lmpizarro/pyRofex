4ed7aa6c6edc037cfe3d9cb2d777be6859f3d202
lmpizarro <lmpizarro@gmail.com>
2023-08-31 21:57:53 -0300

    refactor bonos

--- ratios/gd_al.py
+++ ratios/gd_al.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 
 def ratio_bond_usd(year: str = 29):
-    hist_gd = coti_hist(scrap_bonos_rava(f"gd{year}"))
+    key_gd = f"gd{year}"
+    hist_gd = coti_hist(scrap_bonos_rava(key_gd))
     key_bond = f"al{year}" if year != 38 else f"ae{year}"
     hist_al = coti_hist(scrap_bonos_rava(key_bond))
 
@@ -18,28 +19,17 @@ def ratio_bond_usd(year: str = 29):
     mrg[f"ratio_{year}"] = mrg[f"cierre_gd{year}"] / mrg[f"cierre_al{year}"]
     mrg[f"ratio_usd_{year}"] = mrg[f"usd_cierre_gd{year}"] / mrg[f"usd_cierre_al{year}"]
 
-    mrg[f"ewm_ratio_{year}"] = mrg[f"ratio_{year}"].ewm(alpha=0.1).mean()
-    mrg[f"z_signal_{year}"] = mrg[f"ratio_{year}"] - mrg[f"ewm_ratio_{year}"]
-    mrg[f"ewm_z_signal_{year}"] = mrg[f"z_signal_{year}"].ewm(alpha=0.25).mean()
+    mrg["fecha"] = pd.to_datetime(mrg["fecha"], format="%Y-%m-%d").dt.date
+    mrg.set_index("fecha", inplace=True)
 
-    print(f"ratio {year} mean", mrg[f"ratio_{year}"].mean())
-    st_dev = mrg[f"z_signal_{year}"].std()
-    print(f"ratio last ", mrg.iloc[-1][f"ratio_{year}"])
-
-    plt.title(f"g_{year}:al_{year}")
-    plt.plot(mrg[f"z_signal_{year}"])
-    plt.plot(mrg[f"ewm_z_signal_{year}"])
-
-    plt.axhline(y=0.0, color="k", linestyle="-")
-    plt.axhline(y=1.5 * st_dev, color="y", linestyle="-")
-    plt.axhline(y=st_dev, color="r", linestyle="-")
-    plt.axhline(y=-st_dev, color="g", linestyle="-")
-    plt.axhline(y=-1.5 * st_dev, color="y", linestyle="-")
+    plt.plot(mrg[f"ratio_usd_{year}"])
+    plt.title(f"{key_gd}/{key_bond}")
     plt.show()
 
 
 def ratios_bonos_dolar():
-    for y in [29, 30, 35, 38, 41]:
+    years =[29, 30, 35, 38, 41]
+    for y in years:
         ratio_bond_usd(y)
 
 
