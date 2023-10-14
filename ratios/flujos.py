fc3a7af21c03790fd39e33325e70003cc007f8b3
lmpizarro <lmpizarro@gmail.com>
2023-10-14 16:28:04 -0300

    refactor

--- ratios/flujos.py
+++ ratios/flujos.py
@@ -8,10 +8,8 @@ from rava import ultima_coti_bono_mep
 
 from dataclasses import dataclass
 
-print('ba37d en dolares: ', ultima_coti_bono_mep())
-
-
-def transform_ba37d():
+keys = ['FECHA', 'SALDO', 'CUPÓN', 'AMORTIZACIÓN', 'TOTAL']
+def flujo_ba37d():
     df = pd.read_csv('datos/flujoFondos_BA37D.csv')
     df = df[['Fecha de pago', 'Renta', 'Amortización', 'R+A']]
     df = df.rename({'Fecha de pago': 'FECHA', 'Renta': 'CUPÓN', 'Amortización': 'AMORTIZACIÓN', 'R+A': 'TOTAL'}, axis=1)
@@ -20,58 +18,68 @@ def transform_ba37d():
     total = df.TOTAL.sum()
     df['SALDO'] = 0
     df["FECHA"] = pd.to_datetime(df["FECHA"], format="%d/%m/%Y").dt.date
-    df = df[['FECHA', 'SALDO', 'CUPÓN', 'AMORTIZACIÓN', 'TOTAL']]
+    df = df[keys]
     df['ticker'] = 'BA37D'
 
-    limit_date = datetime.now().date()
-    df = change_index(df, key='FECHA')
-    df = df[df.index > limit_date]
+    buy_date = datetime.now().date()
+    # df = change_index(df, key='FECHA')
+    df = df[df.FECHA > buy_date]
 
-
-    data = {'SALDO': 0, 'CUPÓN': 0, 'AMORTIZACIÓN': 0, 'TOTAL': -ultima_coti_bono_mep(), 'ticker': 'BA37D'}
-    df = pd.concat([pd.DataFrame(data=data, index=[limit_date+timedelta(days=2)]), df])
+    ult_precio = ultima_coti_bono_mep()
+    data = {'SALDO': 0, 'CUPÓN': -ult_precio, 'AMORTIZACIÓN': 0, 'TOTAL': -ult_precio, 'ticker': 'BA37D'}
+    df = pd.concat([pd.DataFrame(data=data, index=[buy_date+timedelta(days=2)]), df])
+    df = df[['ticker']+keys]
     df['acumulado'] = df.TOTAL.cumsum()
+    print(ult_precio)
     return df
 
 
 @dataclass
 class BonoArg:
-    # precio: float
-    # ticker: str
-    # paridad: str
-    # vt: str
-    # tir: str
-    # md: str
-    calendar: pd.DataFrame
+    precio: float
+    ticker: str
+    paridad: str
+    vt: str
+    tir: str
+    md: str
+    # flujos: pd.DataFrame
 
-maps_variables = {'Precio': 1, 'ticker': 0, 'Paridad': 3, 'Valor Técnico': 4, 'TIR': 5, 'MD': 6}
+def get_other_values(soup):
+    values = {}
+    maps_variables = {'precio': 1, 'ticker': 0, 'paridad': 3, 'vt': 4, 'tir': 5, 'md': 6}
+    for k, v in maps_variables.items():
+        values[k] = soup[0].iloc[v]['Valor']
+    return BonoArg(**values)
+
+def flujo_bono_soberano(year):
+    ticker_for_query = f"AL{year}D" if year != 38 else f'AE{year}D'
+    ticker_for_query = f"GD{year}D" if year == 46 else ticker_for_query
+    url = f"https://bonistas.com/bonos-argentinos/{ticker_for_query}"
+    soup = pd.read_html(url)
+    df_flujo = soup[3]
+    df_flujo["FECHA"] = pd.to_datetime(df_flujo["FECHA"], format="%Y/%m/%d").dt.date
+    ult_precio = -float(soup[0].iloc[1]['Valor'])
+    buy_date =datetime.now().date()
+    line = dict(zip(keys, [buy_date+timedelta(days=2), 0, ult_precio, 0, ult_precio] ))
+    line_df = pd.DataFrame.from_records([line])
+    df_flujo = pd.concat([line_df, df_flujo])
+    df_flujo["ticker"] = ticker_for_query
+    df_flujo = df_flujo[['ticker']+ keys]
+    df_flujo['acumulado'] = df_flujo['TOTAL'].cumsum()
+
+    print(df_flujo[df_flujo['acumulado'] > 0].head(1))
+    print(ult_precio)
+
+    return df_flujo
+
 def main():
-    years =[(29,1), (30,1), (35,1), (38,1), (41,1), (46,1)]
-    keys = ['SALDO', 'CUPÓN', 'AMORTIZACIÓN', 'TOTAL']
-    flujos = transform_ba37d()
-    for e in years:
-        year = e[0]
-        bond = f"AL{year}D" if year != 38 else f'AE{year}D'
-        bond = f"GD{year}D" if year == 46 else bond
-        url = f"https://bonistas.com/bonos-argentinos/{bond}"
-        soup = pd.read_html(url)
-        df_flujo = soup[3]
-        df_flujo["FECHA"] = pd.to_datetime(df_flujo["FECHA"], format="%Y/%m/%d").dt.date
-        values = {}
-        for k, v in maps_variables.items():
-            values[k] = soup[0].iloc[v]['Valor']
+    years =[29, 30, 35, 38, 41, 46]
+    df_flujo = flujo_ba37d()
+    print(df_flujo[df_flujo['acumulado'] > 0].head(1))
+    flujos = df_flujo
+    for year in years:
 
-        precio = -float(soup[0].iloc[1]['Valor'])
-        line = dict(zip(['FECHA']+ keys, [datetime.now().date(), 0, precio, 0, precio] ))
-        line_df = pd.DataFrame.from_records([line])
-        df_flujo = pd.concat([line_df, df_flujo])
-        df_flujo['acumulado'] = df_flujo['TOTAL'].cumsum()
-        print(bond)
-        print(df_flujo[df_flujo['acumulado'] > 0].head(1))
-
-        df_flujo[keys] = e[1] * df_flujo[keys]
-        df_flujo["ticker"] = bond
-
+        df_flujo = flujo_bono_soberano(year=year)
         flujos = pd.concat([flujos, df_flujo])
 
     # print(flujos.head())
