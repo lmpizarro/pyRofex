14df2738252d8190ed710e0885eb0c743fa8e600
lmpizarro <lmpizarro@gmail.com>
2024-01-24 17:56:38 -0300

    std mean

--- ratios/utils.py
+++ ratios/utils.py
@@ -65,3 +65,40 @@ def regressor(df_al_gd: pd.DataFrame, key='ratio'):
     regress = list(map(regress_func, x))
     return x, y, regress
 
+def genDates():
+    import calendar
+    import datetime
+    from datetime import timedelta
+    import yfinance as yf
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    assets = {'SPY':{'colour': 'g-'}, 'AAPL':{'colour': 'ko-'}, 'TSLA':{'colour': 'rx-'}}
+    colours = {'AAPL': 'rx-', 'TSLA': 'ko-', 'SPY': 'g-'}
+
+    print(calendar.monthrange(2023, 12))
+    today = datetime.datetime.now().date()
+    sixmonths = timedelta(days=180)
+
+    endDates = [(datetime.date(y, m, calendar.monthrange(y, m)[1]) - sixmonths, datetime.date(y, m, calendar.monthrange(y, m)[1]))  for y in range(2011, 2025) for m in range(1, 13) if datetime.date(y, m, calendar.monthrange(y, m)[1]) < today]
+    df = yf.download(list(assets.keys()), start=endDates[0][0])['Adj Close']
+    returns = np.log(df/df.shift(1))
+    stds = pd.DataFrame()
+    for rng in endDates:
+        sdf = returns.loc[rng[0]:rng[1]]
+        meanD = {}
+        for asset in assets:
+            meanD['asset'] = asset
+            meanD['mean']  = sdf[asset].mean()
+            meanD['std']   = sdf[asset].std()
+            meanD['date'] = rng[1]
+            df2 =pd.DataFrame.from_dict([meanD])
+            stds = pd.concat([stds, df2 ], ignore_index=True)
+    for asset in assets:
+        assetdata = stds[stds.asset==asset].set_index('date')
+        plt.plot(assetdata['std'], assets[asset]['colour'])
+    plt.show()
+genDates()
+
+
